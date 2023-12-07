b486eed19ca594eb21982869fde1f7c5dfbef9f4
Gisela <giselamata27@gmail.com>
2023-12-07 13:53:53 -0500

    Added more questions and have tested that they work.

--- index.js
+++ index.js
@@ -4,15 +4,25 @@ inquirer
     .prompt([
         {
             type: 'input',
-            name: 'color',
-            message: 'What is your prefered color?',
-    },
+            name: 'backgroundcolor',
+            message: 'What would you want the background of your logo color to be?',
+        },
+        {
+            type: 'input',
+            name: 'textcolor',
+            message: 'What color do you want your font to be?',
+        },
         {
             type: 'rawlist',
             name: 'shapes',
             message: 'What is your prefered shape?',
             choices: ['Square', 'Circle', 'Triangle'],
-    },
+        },
+        {
+            type: 'input',
+            name: 'logoletters',
+            message: 'What color do you want your three letter logo to be?',
+        },
     ])
     .then((answers) => {
         console.info('Answers: ', answers);
@@ -20,6 +30,6 @@ inquirer
         init();
     });
 
-    function init() {
-        console.log('Initialization logic goes here')
-    }
+function init() {
+    console.log('Initialization logic goes here')
+}
