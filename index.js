a1bd85165f4a3a78cffffbc9addd65fde8892438
Gisela <giselamata27@gmail.com>
2023-12-07 13:45:21 -0500

    Adding features to the list items.

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ import inquirer from 'inquirer';
 inquirer
     .prompt([
         {
-            type: 'list',
+            type: 'input',
+            name: 'color',
+            message: 'What is your prefered color?',
+    },
+        {
+            type: 'rawlist',
             name: 'shapes',
             message: 'What is your prefered shape?',
             choices: ['Square', 'Circle', 'Triangle'],
     },
     ])
-    .then(answers => {
-        console.info('Answer: ', answers.shapes);
+    .then((answers) => {
+        console.info('Answers: ', answers);
+        //Calling the init function
+        init();
     });
+
+    function init() {
+        console.log('Initialization logic goes here')
+    }
