1e913515506d360305a12a66713dda6a1c8e9690
Gisela <giselamata27@gmail.com>
2023-12-11 14:09:04 -0500

    Adding in a function to write file from the svg shapes the user inputs

--- index.js
+++ index.js
@@ -5,4 +5,18 @@ import inquirer from 'inquirer';
 import fs from 'fs';
 
 // Importing classes from lib/shapes directory
-const { Triangle, Square, Circle } = require("./lib/shapes")
+const { Circle, Triangle, Square } = require("./lib/shapes")
+
+// Function to write the SVG file using users answers
+function writeTofile(fileName, answers) {
+    // File starts an an empty string
+    let svgString = "";
+    // Set the width and height of logo container
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    // <g> tag wraps <text> tag so that the user three letter initals are on top of shape
+    svgString += "<g>";
+    // Takes user input for shape choice and inserts it into SVG file
+    svgString += `${answers.shape}`;
+
+    
+}
